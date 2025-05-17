10fb9dea9eb2c395983ce5293e5b251b80a0fc07
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-05-18 01:17:37 +0530

    initial commit

---
+++ server/src/app.config.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const corsConfig = {
+  origin: `http://localhost:${process.env.CLIENT_PORT}`,
+  credentials: true,
+};
+
+export const backupPort = 4000;
+
+export const jwtKey = process.env.JWT_SECRET || 'dev_fallback_71c2b94e8a1045d28c8a3d2c905fa7b34bde1e6c5f0f4993b02174c9f9e16a3f';
